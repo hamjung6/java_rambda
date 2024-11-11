2408591e880d47764bcdc285ff2bedd021191901
WIN-51BV20CU4GG\ICT02_16 <hamjunblack6@gmail.com>
2024-11-11 10:18:42 +0900

    쓰레드 완료

---
+++ java_고급_문법/src/ThreadEx1Main.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class ThreadEx1Main {
+
+   public static void main(String[] args) {
+
+      List<Student> sArr = new ArrayList<Student>();
+      sArr.add(new Student(90, 80, 70, "홍길동"));
+      sArr.add(new Student(80, 60, 70, "홍길순"));
+      sArr.add(new Student(70, 60, 70, "홍말자"));
+      sArr.add(new Student(50, 50, 50, "바둑이"));
+
+      sArr.stream().filter(student -> student.name.startsWith("홍"))
+            .mapToDouble(student -> (student.kor + student.eng + student.math) / 3.0)
+            .forEach(avg -> System.out.println(avg));
+
+      List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+
+      //Optional<Integer> sum = numbers.stream().reduce((x, y) -> x + y);
+      Optional<Integer> sum = numbers.stream().reduce(Integer::sum);
+      
+      sum.ifPresent(s -> System.out.println("sum = " + s));
+   }
+}
